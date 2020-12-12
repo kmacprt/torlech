b53b1965d297301d0f631530189caa7fc7763aa7
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-12 16:01:35 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -9,4 +9,4 @@ class Config(Config):
   RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 1ugGQR35PZbG5SjNv53XGWvlj3LZO8aCP\ntoken = {"access_token":"ya29.a0AfH6SMBb1KthGGArvJDOHuOy_Vew_BPVWG34NDKwkQ1ygVWwt-OIhh5x7y98WKbV95n5m4Xbnnr-fcBt9rLv_HxKs25HG_ScWXpi1OtSw3rg19Y6JNnoTsheCgT9IYvdyhKYFruWFZR680ODVgzBSQdPeQvUgXm3CRXLVlgCxng","token_type":"Bearer","refresh_token":"1//09m-3YTLywwFtCgYIARAAGAkSNwF-L9IrXUc3DNs3d_BgskfZ2QYYjcGIzCvITifoht-Xd8VbZp_rxQbd6eHg2XvLrp2KBk6hGt8","expiry":"2020-12-12T16:52:08.8696738+05:30"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
     #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
-  INDEX_LINK = "https://torhunter.darkkali614.workers.dev/Gdrive bot  
+  INDEX_LINK = "https://torhunter.darkkali614.workers.dev/Gdrive bot"  
