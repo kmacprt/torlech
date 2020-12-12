667d4bab63b782bc5b589ffa2597bfb24c9543df
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-12 15:34:26 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,12 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1436730490:AAGOxPmKhXdB0SgKk-o6v91vc-Lf3uv8UwI"
+  APP_ID = 1383845
+  API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
+  AUTH_CHANNEL = [-1001266398622]
+  DESTINATION_FOLDER = "DOWNLOAD" #Name of your folder read readme(not id of the folder)
+  RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 1FoemGATbwi2dBVitzP3DkCj2uq_anNan\ntoken = {"access_token":"ya29.a0AfH6SMC9ZTaAFI9J67nrVc-_P3P8Mh73D7gvGzcCdoSutZH3rJtv8Zd6Ec2ZghUESpPDAaBQzZBk-CQ1NpDeN7pd0Mn4upY8lJJxh4NB8fWoCXyhAwW5JW7JzDWJ4QEdbhBcV7ipI90tRObk49X0c-nzh-IhppNoUVoPfdniCkg","token_type":"Bearer","refresh_token":"1//0cNaN6be_ZskCCgYIARAAGAwSNwF-L9IrltPBtgutEvyY0BfY6-37unC_4U0bQrAlTuSKsf4RcuLUb7O_S08RDYGzX9F9i11ZD1A","expiry":"2020-12-12T13:54:58.6948751+05:30"}"""
+    #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
+    #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
+  INDEX_LINK = "https://spirit.darkkali614.workers.dev/DOWNLOAD"  
