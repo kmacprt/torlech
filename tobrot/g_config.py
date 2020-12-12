62997b12b6a4e17d5f8d37c92a34d7f9ad8e259b
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-12 16:36:01 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -7,6 +7,6 @@ class Config(Config):
 	API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
 	OWNER_ID = "1131653685" #ID of bot owner
 	AUTH_CHANNEL = [-1001266398622]
-	DESTINATION_FOLDER = "Gdrive bot" #Name of your folder read readme
+	DESTINATION_FOLDER = "Gdrive" #Name of your folder read readme
 	RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 1ugGQR35PZbG5SjNv53XGWvlj3LZO8aCP\ntoken = {"access_token":"ya29.a0AfH6SMBb1KthGGArvJDOHuOy_Vew_BPVWG34NDKwkQ1ygVWwt-OIhh5x7y98WKbV95n5m4Xbnnr-fcBt9rLv_HxKs25HG_ScWXpi1OtSw3rg19Y6JNnoTsheCgT9IYvdyhKYFruWFZR680ODVgzBSQdPeQvUgXm3CRXLVlgCxng","token_type":"Bearer","refresh_token":"1//09m-3YTLywwFtCgYIARAAGAkSNwF-L9IrXUc3DNs3d_BgskfZ2QYYjcGIzCvITifoht-Xd8VbZp_rxQbd6eHg2XvLrp2KBk6hGt8","expiry":"2020-12-12T16:52:08.8696738+05:30"}"""
 	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
