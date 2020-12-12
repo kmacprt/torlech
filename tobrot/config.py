25d5fb6d508ae29dae98577131215cd248bd183e
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-12 15:58:42 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -5,8 +5,8 @@ class Config(Config):
   APP_ID = 1383845
   API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
   AUTH_CHANNEL = [-1001423405230]
-  DESTINATION_FOLDER = "TORHUNTER" #Name of your folder read readme(not id of the folder)
-  RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 19JWUztITYFMI4ouFIWxczTj-MsrKBsmw\ntoken = {"access_token":"ya29.a0AfH6SMDAsu_t7bnyVjskDNxsXZGM-VFGV19Y2dliBbDoF0tX8UZY37Y0CIf0tyZwLhaA-fdDW8X95Hx8bg0uQCefZPKm6hXAXjhPCMgG0-ZE0cM50ruv0bBopKVr2fTYxP066DetE_qtX7v6pfkWR7psuavDyiqW7aqIcQTghoE","token_type":"Bearer","refresh_token":"1//09jiQRmiVDpuOCgYIARAAGAkSNwF-L9IrXEuo4u7ep_PD8f5aUYfSOHsPdjQlNJIPPtjQX3DO5KUXJHlNltIiZ1IS7t4w4dDuC9M","expiry":"2020-12-12T16:30:09.8369024+05:30"}\nteam_drive = 0ADOQtaG3VrFxUk9PVA"""
+  DESTINATION_FOLDER = "Gdrive bot" #Name of your folder read readme(not id of the folder)
+  RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 1ugGQR35PZbG5SjNv53XGWvlj3LZO8aCP\ntoken = {"access_token":"ya29.a0AfH6SMBb1KthGGArvJDOHuOy_Vew_BPVWG34NDKwkQ1ygVWwt-OIhh5x7y98WKbV95n5m4Xbnnr-fcBt9rLv_HxKs25HG_ScWXpi1OtSw3rg19Y6JNnoTsheCgT9IYvdyhKYFruWFZR680ODVgzBSQdPeQvUgXm3CRXLVlgCxng","token_type":"Bearer","refresh_token":"1//09m-3YTLywwFtCgYIARAAGAkSNwF-L9IrXUc3DNs3d_BgskfZ2QYYjcGIzCvITifoht-Xd8VbZp_rxQbd6eHg2XvLrp2KBk6hGt8","expiry":"2020-12-12T16:52:08.8696738+05:30"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
     #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
-  INDEX_LINK = "https://torhunter.darkkali614.workers.dev/TORHUNTER"  
+  INDEX_LINK = "https://torhunter.darkkali614.workers.dev/Gdrive bot  
